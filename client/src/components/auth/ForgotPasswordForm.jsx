492256ad1a5f4494a407c704b2819b89252fdbfe
shridharaggrwal <agrawalshreedhar8@gmail.com>
2025-07-10 15:40:19 +0530

    updated url in forgatpassword

--- client/src/components/auth/ForgotPasswordForm.jsx
+++ client/src/components/auth/ForgotPasswordForm.jsx
@@ -14,7 +14,7 @@ const ForgotPasswordForm = ({ userType = 'staff' }) => {
   const checkServerRoutes = async () => {
     try {
       // Check if server is running
-      const rootResponse = await axios.get('http://localhost:8080/');
+      const rootResponse = await axios.get('https://shivhospital.onrender.com/');
       console.log('Server root response:', rootResponse.status);
       
       // This is just for debugging
